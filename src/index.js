4b4b5192c6bea5ee13e50601a444078ae32fb369
Svit-Zh <ekohimias@gmail.com>
2022-10-03 20:35:33 +0200

    updated forecast functions

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function formatDate(timestamp) {
     "December",
   ];
   let month = months[currentMonth];
-  let year = date.getFullYear();
+  let year = date.getFullYear(timestamp);
 
   return `${month}, ${now}, ${year}`;
 }
@@ -53,9 +53,9 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForcast(response) {
+function displayForecast(response) {
   let forecast = response.data.daily;
-  let forcastElement = document.querySelector("#forecast");
+  let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="col-sm">`;
 
   forecast.forEach(function (forecastDay, index) {
@@ -87,7 +87,7 @@ function displayForcast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "ab6174be7b717732ef179b1d3f3555cf";
+  let apiKey = "e947cb2640f1db92e6a19005bc43b435";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
