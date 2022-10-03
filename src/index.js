ba4cd2eb0c7d827f01475ab5c77d478031064fbb
Svit-Zh <ekohimias@gmail.com>
2022-10-03 21:05:42 +0200

    finalised the col

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `<div class="col-2">
+        `<div class="col-2" id="forecast1">
               
                   <div class="dayOfWeek">${formatDay(forecastDay.dt)}</div>
                    <img src="http://openweathermap.org/img/wn/${
